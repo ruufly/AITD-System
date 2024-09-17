334743a271d5c40d614cc5975235052eb3cddac0
distjr_ <60539875+ruufly@users.noreply.github.com>
2024-09-17 16:09:31 +0800

    Update xerlist.py

--- aitd/xerlist.py
+++ aitd/xerlist.py
@@ -14,6 +14,31 @@ class ComparatorList(object):
         setattr(self, name, comparator)
 
 
+class TreePlanterList(object):
+    def __init__(self):
+        pass
+    def add(self, name, treePlanter):
+        setattr(self, name, treePlanter)
+
+class ProcessorList(object):
+    def __init__(self):
+        pass
+    def add(self, name, processor):
+        setattr(self, name, processor)
+
+
+class DisplayList(object):
+    def __init__(self):
+        pass
+    def add(self, name, display):
+        setattr(self, name, display)
+
 ParserList = ParserList()
 
 ComparatorList = ComparatorList()
+
+TreePlanterList = TreePlanterList()
+
+ProcessorList = ProcessorList()
+
+DisplayList = DisplayList()
