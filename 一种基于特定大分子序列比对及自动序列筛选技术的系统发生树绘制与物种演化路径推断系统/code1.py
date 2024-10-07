14f65f86689137539c0ab2e71f73c22b51438251
distjr_ <60539875+ruufly@users.noreply.github.com>
2024-10-07 10:13:40 +0800

    Delete 一种基于特定大分子序列比对及自动序列筛选技术的系统发生树绘制与物种演化路径推断系统 directory

--- 一种基于特定大分子序列比对及自动序列筛选技术的系统发生树绘制与物种演化路径推断系统/code1.py
+++
@@ -1,7 +0,0 @@
-with open(os.path.join(projectpath, "setting.json"), "r") as f:
-pjset = json.load(f)
-
-for i in pjset:
-    if i in ["sequence_list", "tree_list", "sketch_list", "alignment_list"]:
-        for j in pjset[i]:
-            namespaces[j] = pjset[i][j]
